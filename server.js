12be32040af6b89149f4629591dde0e0ee322192
AwaisBinKaleem <mohammadawais0131@gmail.com>
2023-04-03 22:30:39 +0500

    mockdata and cors

--- server.js
+++ server.js
@@ -1,21 +1,44 @@
 const express = require("express");
+const chance = require("chance")();
+const cors = require("cors");
 const app = express();
 
 const services = require("./services");
 
-data =
-  "data can be in any formate, string,integers,objects,lists,json,files,html etc";
+app.use(cors());
+app.use(express.json());
+
+// data =
+//   "data can be in any formate, string,integers,objects,lists,json,files,html etc";
+
+let response = [];
+let id = 0;
 
 app.get("/", (req, res) => {
   res.status(200).send("welcome to node js server.powered py express js");
 });
 
-app.get("/getData", (req, res) => {
-  res.status(200).send(data);
+app.get("/getData", async (req, res) => {
+  for (let i = 1; i <= 10; i++) {
+    id++;
+    response.push({
+      id: id,
+      name: chance.name(),
+      email: chance.email(),
+      address: {
+        city: chance.city(),
+        country: chance.country(),
+      },
+      contact: {
+        phone: chance.phone(),
+      },
+    });
+  }
+  await res.status(200).send(response);
 });
 
 app.get("/login", (req, res) => {
-  const resData = services.loginToUser();
+  const resData = services.signup();
   res.status(200).send(resData);
 });
 
