3b0652633ddb8eab5db1fc26bbf243f341e8db01
awais-kaleem-blocksdev <awais.kaleem@blocksdev.com>
2023-12-22 04:32:48 +0500

    fix

--- server.js
+++ server.js
@@ -53,7 +53,6 @@ const getMockData = () => {
 app.get("/", async (req, res) => {
   const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
   console.log(geoip.lookup(ip))
-  // console.log(geoip.lookup('39.45.6.216'))
   res.render("welcome", {
     greetings: "welcome to node js server.powered py express js"
   });
